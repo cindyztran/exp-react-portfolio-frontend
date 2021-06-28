2f8cf2eafa82e68c4f292ea46b83da733f3d7b03
cindytran <cindyztran@gmail.com>
2021-06-28 12:01:05 -0700

    frontend complete

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+
+//Import Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Route, Switch} from "react-router-dom";
+
+//Import Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+
+function App() {
+  //URL
+  const URL = 'https://backend-czt.herokuapp.com/';
+
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/projects">
+          <Projects URL={URL}/>
+        </Route>
+        <Route path="/about">
+          <About URL={URL}/>
+        </Route>
+      </Switch>
+      <Footer/>
+   
+    </div>
+  );
+}
+
+export default App;
